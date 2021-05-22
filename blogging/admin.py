81118ded7785142cdb6149e7ab98f0edf0faafe9
Sopeaktra_Danh <Sopeaktra.Danh@gmail.com>
2021-05-22 16:31:17 -0700

    Custom ModelAdmin interface for Post & Category. Can now edit Categories in Post admin

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    fields = ('title', 'author', 'published_date', 'text')
+    inlines = [CategoryInline,]
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+    inlines = [CategoryInline,]
+    pass
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
