17ba97524c180e9e293b8f16a07ac785f9e783a4
Sopeaktra_Danh <Sopeaktra.Danh@gmail.com>
2021-06-06 19:02:03 -0700

    Applied black.

--- polling/views.py
+++ polling/views.py
@@ -7,12 +7,12 @@ from django.views.generic.detail import DetailView
 
 class PollListView(ListView):
     model = Poll
-    template_name = 'polling/list.html'
+    template_name = "polling/list.html"
 
 
 class PollDetailView(DetailView):
     model = Poll
-    template_name = 'polling/detail.html'
+    template_name = "polling/detail.html"
 
     def post(self, request, *args, **kwargs):
         poll = self.get_object()
@@ -25,7 +25,8 @@ class PollDetailView(DetailView):
 
         context = {"object": poll}
         return render(request, "polling/detail.html", context)
-        
+
+
 # Create your views here.
 # def list_view(request):
 #     context = {'polls': Poll.objects.all()}
